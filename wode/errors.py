081d2c79780182d93637e896ca35e6769c7e9727
Hannes <h@nnes.dev>
2022-09-25 21:19:40 +0100

    Store error info as properties of class

--- wode/errors.py
+++ wode/errors.py
@@ -10,10 +10,13 @@ class WodeErrorType(Enum):
 
 class WodeError:
     def __init__(self, error_type: WodeErrorType, source: str, position: int) -> None:
-        line_number = get_line_number_of_position(source, position)
-        line = get_line(source, line_number)
-        position_in_line = get_position_in_line(source, position)
-        arrow_string = position_in_line * " " + "^"
+        self.error_type = error_type
+        self.source = source
+        self.position = position
+        self.line_number = get_line_number_of_position(source, position)
+        self.line = get_line(source, self.line_number)
+        self.position_in_line = get_position_in_line(source, position)
+        arrow_string = self.position_in_line * " " + "^"
         self.message = "\n".join(
-            [f"{error_type} on line {line_number}:", line, arrow_string]
+            [f"{error_type} on line {self.line_number}:", self.line, arrow_string]
         )
