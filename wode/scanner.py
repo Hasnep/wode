b6da282326c19c4f5f908f645deff85a1931b079
Hannes <h@nnes.dev>
2022-10-22 01:41:23 +0100

    WIP

--- wode/scanner.py
+++ wode/scanner.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Callable, List, Tuple
+from typing import Any, Callable, List, Set, Tuple
 
 from koda import Err, Just, Maybe, Ok, Result, mapping_get, nothing
 
@@ -21,6 +21,7 @@ class Source:
 
 
 ScannerOutput = Maybe[Tuple[str, Source]]
+Scanner = Callable[[Source], ScannerOutput]
 
 
 def chomp(source: Source, n: int = 1) -> ScannerOutput:
@@ -32,364 +33,106 @@ def chomp(source: Source, n: int = 1) -> ScannerOutput:
     return Just((first_n_characters, remaining_source))
 
 
-# def peek(source: str, n: int = 1) -> ScannerOutput:
-#     """Return the first n characters and the source."""
-#     if len(source) < n:
-#         return nothing
-#     return Just((source[:n], source))
-
-
-def scan_literal(source: Source, value: str) -> ScannerOutput:
-    match chomp(source, len(value)):
-        case Just((first_n_characters, remaining_source)):
-            if first_n_characters == value:
-                return Just((first_n_characters, remaining_source))
-            else:
-                return nothing
-        case _:
-            return nothing
-
-
-def scan_literals(source: Source, values: List[str]) -> ScannerOutput:
-    # Scan the longest tokens first
-    values = sorted(values, key=len, reverse=True)
-    for value in values:
-        match scan_literal(source, value):
-            case Just(x):
-                return Just(x)
-            case _:
-                pass
-    return nothing
-
-
-def scan_while_callback(
-    source: Source, continue_callback: Callable[[str], Maybe[bool]]
-) -> ScannerOutput:
-    token_length = 1
-    while True:
-        match chomp(source, token_length):
-            case Just((first_n_characters, _)):
-                match continue_callback(first_n_characters):
-                    case Just(callback_output):
-                        if callback_output:
-                            token_length += 1
-                        else:
-                            return chomp(source, token_length - 1)
-                    case _:
-                        return nothing
+def get_literal_scanner(value: str) -> Scanner:
+    def literal_scanner(source: Source) -> ScannerOutput:
+        match chomp(source, len(value)):
+            case Just((first_n_characters, remaining_source)):
+                if first_n_characters == value:
+                    return Just((first_n_characters, remaining_source))
+                else:
+                    return nothing
             case _:
                 return nothing
 
+    return literal_scanner
 
-def scan_delimited(
-    source: Source, left_delimiter: str, right_delimiter: str
-) -> ScannerOutput:
-    middle_bite_size = 0
-    while True:
-        the_source = source
 
-        # Try matching the left delimiter
-        match scan_literal(the_source, left_delimiter):
+def get_while_scanner(continue_callback: Callable[[str], Maybe[bool]]) -> Scanner:
+    def while_scanner(source: Source) -> ScannerOutput:
+        token_length = 1
+        while True:
+            match chomp(source, token_length):
+                case Just((bite, _)):
+                    last_character = bite[-1]
+                    match continue_callback(last_character):
+                        case Just(callback_output):
+                            if callback_output:
+                                token_length += 1
+                            else:
+                                return chomp(source, token_length - 1)
+                        case _:
+                            return nothing
+                case _:
+                    return nothing
+
+    return while_scanner
+
+
+def get_while_is_any_literal_scanner(continue_values: Set[str]) -> Scanner:
+    return get_while_scanner(lambda c: Just(c in continue_values))
+
+
+def get_while_is_not_any_literal_scanner(terminate_values: Set[str]) -> Scanner:
+    return get_while_scanner(lambda c: Just(c not in terminate_values))
+
+
+def get_delimited_scanner(
+    scan_left_delimiter: Scanner, scan_middle: Scanner, scan_right_delimiter: Scanner
+) -> Scanner:
+    def delimited_scanner(source: Source) -> ScannerOutput:
+        match scan_left_delimiter(source):
             case Just((_, remaining_source)):
-                the_source = remaining_source
+                source = remaining_source
             case _:
                 return nothing
 
-        # Try matching the middle section
-        match chomp(the_source, middle_bite_size):
-            case Just((middle_bite, remaining_source)):
-                middle_bite = middle_bite
-                the_source = remaining_source
+        match scan_middle(source):
+            case Just((middle_bite_, remaining_source)):
+                middle_bite = middle_bite_
+                source = remaining_source
             case _:
-                middle_bite_size += 1
-                continue
+                return nothing
 
-        # Try matching the right delimiter
-        match scan_literal(the_source, right_delimiter):
-            case Just((_, remaining_source_again_again)):
-                return Just((middle_bite, remaining_source_again_again))
+        match scan_right_delimiter(source):
+            case Just((_, remaining_source)):
+                source = remaining_source
             case _:
-                middle_bite_size += 1
-                continue
+                return nothing
+
+        return Just((middle_bite, source))
+
+    return delimited_scanner
 
 
+def get_sequence_scanner(scanners: List[Scanner]) -> Scanner:
+    def sequence_scanner(source: Source) -> ScannerOutput:
+        output = ""
+
+        for scanner in scanners:
+            match scanner(source):
+                case Just((bite, remaining_source)):
+                    output += bite
+                    source = remaining_source
+                case _:
+                    return nothing
+
+        return Just((output, source))
+
+    return sequence_scanner
+
+
+# Actual scanners
 def scan_string(source: Source) -> ScannerOutput:
-    return scan_delimited(source, '"', '"')
+    return get_delimited_scanner(
+        get_literal_scanner('"'),
+        get_while_is_not_any_literal_scanner({'"'}),
+        get_literal_scanner('"'),
+    )(source)
 
 
 def scan_comment(source: Source) -> ScannerOutput:
-    return scan_delimited(source, "#", "\n")
-
-
-# def scan_multiple(source,scanner)
-
-
-# class Scanner:
-#     def __init__(self, source: str) -> None:
-#         self.source = source
-#         self.current_position: int = 0
-#         self.tokens: List[Token] = []
-
-#     def get_remaining_source(self) -> str:
-#         return self.source[self.current_position :]
-
-#     def is_at_end(self) -> bool:
-#         return self.current_position == len(self.source)
-
-#     def advance(self, by: int = 1) -> None:
-#         self.current_position += by
-
-#     def look_one(self) -> str:
-#         return self.source[self.current_position]
-
-#     def look_ahead(self) -> Maybe[str]:
-#         try:
-#             return Just(self.source[self.current_position + 1])
-#         except IndexError:
-#             return nothing
-
-#     def scan_for_end_of_file_token(self) -> Maybe[Token]:
-#         if not self.is_at_end():
-#             return nothing
-
-#         return Just(Token(TokenType.EOF, "", self.current_position))
-
-#     def scan_for_whitespace_token(self) -> Maybe[str]:
-#         first_character = self.get_remaining_source()[:1]
-#         if first_character not in [" ", "\t", "\n"]:
-#             return nothing
-
-#         return Just(first_character)
-
-#     def scan_for_comment_token(self) -> Maybe[Token]:
-#         if self.look_one() != "#":
-#             return nothing
-
-#         comment = ""
-#         while not self.is_at_end() and self.look_one() != "\n":
-#             comment += self.look_one()
-#             self.advance()
-#         return Just(Token(TokenType.COMMENT, comment, self.current_position))
-
-#     def scan_for_string_token(self) -> Result[Maybe[Token], WodeError]:
-#         if self.look_one() != '"':
-#             return Ok(nothing)
-
-#         n_quotation_marks_seen = 0
-#         string = ""
-#         while n_quotation_marks_seen < 2:
-#             if self.is_at_end():
-#                 return Err(
-#                     WodeError(
-#                         WodeErrorType.UnexpectedEndOfFileError,
-#                         self.source,
-#                         self.current_position - 1,
-#                     )
-#                 )
-#             if self.look_one() == '"':
-#                 n_quotation_marks_seen += 1
-#             else:
-#                 string += self.look_one()
-#             self.advance()
-#         return Ok(Just(Token(TokenType.STRING, string, self.current_position)))
-
-#     def scan_for_double_character_token(self) -> Maybe[Token]:
-#         lexeme = self.get_remaining_source()[:2]
-#         maybe_token_type = mapping_get(double_character_token_mapping, lexeme)
-#         maybe_token = maybe_token_type.map(
-#             lambda token_type: Token(token_type, lexeme, self.current_position)
-#         )
-#         return maybe_token
-
-#     def scan_for_single_character_token(self) -> Maybe[Token]:
-#         lexeme = self.get_remaining_source()[:1]
-#         maybe_token_type = mapping_get(single_character_token_mapping, lexeme)
-#         maybe_token = maybe_token_type.map(
-#             lambda token_type: Token(token_type, lexeme, self.current_position)
-#         )
-#         return maybe_token
-
-#     def scan_for_number_token(self) -> Result[Maybe[Token], WodeError]:
-#         def is_digit(c: str) -> bool:
-#             return c in DIGITS
-
-#         c = self.look_one()
-#         if not is_digit(c):
-#             if c == ".":
-#                 self.advance()
-#                 return Err(
-#                     WodeError(
-#                         WodeErrorType.NoLeadingZeroOnFloatError,
-#                         self.source,
-#                         self.current_position,
-#                     )
-#                 )
-#             else:
-#                 return Ok(nothing)
-
-#         number = ""
-#         while True:
-#             c = self.look_one()
-#             if is_digit(c):
-#                 number += c
-#                 self.advance()
-#             else:
-#                 if c == ".":
-#                     maybe_next_c = self.look_ahead()
-#                     match maybe_next_c:
-#                         case Just(next_c):
-#                             if is_digit(next_c):
-#                                 number += c
-#                                 self.advance()
-#                                 while True:
-#                                     c = self.look_one()
-#                                     if is_digit(c):
-#                                         number += c
-#                                         self.advance()
-#                                     else:
-#                                         return Ok(
-#                                             Just(
-#                                                 Token(
-#                                                     TokenType.FLOAT,
-#                                                     number,
-#                                                     self.current_position,
-#                                                 )
-#                                             )
-#                                         )
-#                             else:
-#                                 self.advance()
-#                                 return Err(
-#                                     WodeError(
-#                                         WodeErrorType.UnterminatedFloatError,
-#                                         self.source,
-#                                         self.current_position,
-#                                     )
-#                                 )
-#                         case _:
-#                             return Err(
-#                                 WodeError(
-#                                     WodeErrorType.UnexpectedEndOfFileError,
-#                                     self.source,
-#                                     self.current_position,
-#                                 )
-#                             )
-#                 else:
-#                     return Ok(
-#                         Just(Token(TokenType.INTEGER, number, self.current_position))
-#                     )
-
-#     def scan_for_identifier_token(self) -> Maybe[Token]:
-#         valid_identifier_prefixes = ["_"] + LETTERS
-#         valid_identifier_characters = ["_"] + LETTERS + DIGITS
-#         c = self.look_one()
-#         if c not in valid_identifier_prefixes:
-#             return nothing
-
-#         identifier = ""
-
-#         while True:
-#             c = self.look_one()
-#             if c in valid_identifier_characters:
-#                 identifier += c
-#                 self.advance()
-#             else:
-#                 break
-
-#         if identifier in reserved_keywords.keys():
-#             return Just(
-#                 Token(
-#                     TokenType(reserved_keywords[identifier]),
-#                     identifier,
-#                     self.current_position,
-#                 )
-#             )
-#         else:
-#             return Just(Token(TokenType.IDENTIFIER, identifier, self.current_position))
-
-#     def scan_once(self) -> Result[Maybe[Token], WodeError]:
-#         match self.scan_for_end_of_file_token():
-#             case Just(end_of_file_token):
-#                 return Ok(Just(end_of_file_token))
-#             case _:
-#                 pass
-
-#         match self.scan_for_whitespace_token():
-#             case Just(_):
-#                 self.advance()
-#                 return Ok(nothing)
-#             case _:
-#                 pass
-
-#         match self.scan_for_comment_token():
-#             case Just(_):
-#                 # TODO: Add parsing for comments
-#                 # return Ok(Just(comment_token))
-#                 return Ok(nothing)
-#             case _:
-#                 pass
-
-#         match self.scan_for_string_token():
-#             case Ok(maybe_string_token):
-#                 match maybe_string_token:
-#                     case Just(string_token):
-#                         return Ok(Just(string_token))
-#                     case _:
-#                         pass
-#             case err:
-#                 return err
-
-#         match self.scan_for_number_token():
-#             case Ok(maybe_number_token):
-#                 match maybe_number_token:
-#                     case Just(number_token):
-#                         return Ok(Just(number_token))
-#                     case _:
-#                         pass
-#             case err:
-#                 return err
-
-#         maybe_double_character_token = self.scan_for_double_character_token()
-#         match maybe_double_character_token:
-#             case Just(double_character_token):
-#                 self.advance(2)
-#                 return Ok(Just(double_character_token))
-#             case _:
-#                 pass
-
-#         maybe_single_character_token = self.scan_for_single_character_token()
-#         match maybe_single_character_token:
-#             case Just(single_character_token):
-#                 self.advance()
-#                 return Ok(Just(single_character_token))
-#             case _:
-#                 pass
-
-#         maybe_identifier_token = self.scan_for_identifier_token()
-#         match maybe_identifier_token:
-#             case Just(identifier_token):
-#                 return Ok(Just(identifier_token))
-#             case _:
-#                 pass
-
-#         err = Err(
-#             WodeError(
-#                 WodeErrorType.UnknownCharacterError, self.source, self.current_position
-#             )
-#         )
-#         self.advance()
-#         return err
-
-#     def scan(self) -> Tuple[List[Token], List[WodeError]]:
-#         errors: List[WodeError] = []
-#         while True:
-#             match self.scan_once():
-#                 case Ok(maybe_token):
-#                     match maybe_token:
-#                         case Just(token):
-#                             self.tokens.append(token)
-#                             if token.token_type == TokenType.EOF:
-#                                 return self.tokens, errors
-#                         case _:
-#                             pass
-#                 case Err(error):
-#                     errors.append(error)
+    return get_delimited_scanner(
+        get_literal_scanner("#"),
+        get_while_is_not_any_literal_scanner({"\n"}),
+        get_literal_scanner("\n"),
+    )(source)
--- wode/tests/test_scanner_utils.py
+++ wode/tests/test_scanner_utils.py
@@ -1,12 +1,16 @@
-from koda import Just, Maybe, nothing
+import pytest
+from koda import Just, Maybe, Nothing, nothing
 
 from wode.scanner import (
     Source,
     chomp,
+    get_literal_scanner,
+    get_sequence_scanner,
+    get_while_is_any_literal_scanner,
+    get_while_is_not_any_literal_scanner,
+    get_while_scanner,
     is_digit,
-    scan_delimited,
-    scan_literal,
-    scan_while_callback,
+    is_letter,
 )
 
 
@@ -23,38 +27,65 @@ def test_chomp_returns_nothing_when_it_reaches_eof():
     assert chomp(source) == nothing
 
 
-def test_scan_literal():
+@pytest.mark.parametrize(
+    ",".join(["source", "literal"]),
+    [("This is my string.", "This"), (" hi ", " "), ("hello", "he")],
+)
+def test_get_literal_scanner(source: str, literal: str):
+    scan_literal = get_literal_scanner(literal)
+    output = scan_literal(Source(source))
+    assert isinstance(output, Just)
+    assert output.val[0] == literal
+
+
+def test_get_literal_scanner_returns_nothing_when_no_match_is_found():
     source = Source("This is my string.")
-    literals = ["This", " ", "is"]
-    for literal in literals:
-        match scan_literal(source, literal):
-            case Just((bite, remaining_source)):
-                assert bite == literal
-                source = remaining_source
-            case _:
-                raise ValueError(
-                    f"Unexpected failure scanning for `{literal}` in `{source.text}`."
-                )
+    scan_literal = get_literal_scanner("hello")
+    assert isinstance(scan_literal(source), Nothing)
 
 
-def test_scan_while_callback():
+def test_get_while_scanner():
     source = Source("123xyz")
-
-    def are_all_digits(s: str) -> Maybe[bool]:
-        if len(s) == 0:
-            return nothing
-        return Just(all(is_digit(c) for c in s))
-
-    output = scan_while_callback(source, are_all_digits)
-
+    scan_while_is_digit = get_while_scanner(lambda c: Just(is_digit(c)))
+    output = scan_while_is_digit(source)
     assert isinstance(output, Just)
     assert output.val == ("123", Source("xyz", 3))
 
 
-def test_scan_delimited():
-    for left in ["[", "<--", "left"]:
-        for right in ["]", "-->", "right"]:
-            source = Source(left + "this is my thing" + right)
-            output = scan_delimited(source, left, right)
-            assert isinstance(output, Just)
-            assert output.val == ("this is my thing", Source("", len(source.text)))
+def test_get_while_is_any_literal_scanner():
+    source = Source("cabbages")
+    scan_abcdefg = get_while_is_any_literal_scanner({"a", "b", "c", "d", "e", "f", "g"})
+    output = scan_abcdefg(source)
+    assert isinstance(output, Just)
+    assert output.val == ("cabbage", Source("s", 7))
+
+
+def test_get_while_is_not_any_literal_scanner():
+    source = Source("some words")
+    scan_non_whitespace = get_while_is_not_any_literal_scanner({" ", "\t", "\n"})
+    output = scan_non_whitespace(source)
+    assert isinstance(output, Just)
+    assert output.val == ("some", Source(" words", 4))
+
+
+def test_get_sequence_scanner():
+    source = Source("123&abc!")
+    scan_sequence = get_sequence_scanner(
+        [
+            get_while_scanner(lambda c: Just(is_digit(c))),
+            get_literal_scanner("&"),
+            get_while_scanner(lambda c: Just(is_letter(c))),
+        ]
+    )
+    output = scan_sequence(source)
+    assert isinstance(output, Just)
+    assert output.val == ("123&abc", Source("!", 7))
+
+
+# def test_scan_delimited():
+#     for left in ["[", "<--", "left"]:
+#         for right in ["]", "-->", "right"]:
+#             source = Source(left + "this is my thing" + right)
+#             output = get_delimited_scanner(source, left, right)
+#             assert isinstance(output, Just)
+#             assert output.val == ("this is my thing", Source("", len(source.text)))
