564035e2ca61c95d40135dcd2fabf018fde271e8
Hannes <h@nnes.dev>
2022-12-19 15:07:46 +0800

    Change test cases to Python objects

--- wode/tests/test_1_scanner.py
+++ wode/tests/test_1_scanner.py
@@ -1,20 +1,30 @@
+from typing import Tuple
+
 import pytest
 
 from wode.scanner import Scanner
-from wode.tests.conftest import WodeTestCase, test_cases
+from wode.tests.conftest import WodeTestCase, test_cases_flattened
 from wode.token_type import TokenType
 
-success_cases = [x for x in test_cases if len(x.expected_errors) == 0]
-failure_cases = [x for x in test_cases if len(x.expected_errors) > 0]
+success_cases = [
+    (test_case_name, test_case)
+    for test_case_name, test_case in test_cases_flattened
+    if len(test_case.expected_errors) == 0
+]
+failure_cases = [
+    (test_case_name, test_case)
+    for test_case_name, test_case in test_cases_flattened
+    if len(test_case.expected_errors) > 0
+]
 
 
 @pytest.mark.parametrize(
-    "success_case", success_cases, ids=[x.name for x in success_cases]
+    "success_case", success_cases, ids=[x[0] for x in success_cases]
 )
-def test_scanner_parses_test_cases(success_case: WodeTestCase):
-    test_case_name = success_case.name
-    source = success_case.source
-    expected_tokens = success_case.expected_tokens
+def test_scanner_parses_test_cases(success_case: Tuple[str, WodeTestCase]):
+    test_case_name = success_case[0]
+    source = success_case[1].source
+    expected_tokens = success_case[1].expected_tokens
 
     # Scan the source
     tokens, errors = Scanner(source).scan()
@@ -25,21 +35,24 @@ def test_scanner_parses_test_cases(success_case: WodeTestCase):
 
     # Test the tokens were scanned as expected
     tokens_simplified = [(token.token_type, token.lexeme) for token in tokens]
-    assert tokens_simplified == (
-        expected_tokens + [(TokenType.EOF, "")]
+    expected_tokens_simplified = [
+        (token.token_type, token.lexeme) for token in expected_tokens
+    ] + [(TokenType.EOF, "")]
+    assert (
+        tokens_simplified == expected_tokens_simplified
     ), f"Test case `{test_case_name}` was not scanned correctly."
 
-    if success_case.broken:
+    if success_case[1].broken:
         pytest.xfail()
 
 
 @pytest.mark.parametrize(
-    "failure_case", failure_cases, ids=[x.name for x in failure_cases]
+    "failure_case", failure_cases, ids=[x[0] for x in failure_cases]
 )
-def test_scanner_fails_on_failure_cases(failure_case: WodeTestCase):
-    test_case_name = failure_case.name
-    source = failure_case.source
-    expected_error_types = failure_case.expected_errors
+def test_scanner_fails_on_failure_cases(failure_case: Tuple[str, WodeTestCase]):
+    test_case_name = failure_case[0]
+    source = failure_case[1].source
+    expected_error_types = failure_case[1].expected_errors
 
     _tokens, errors = Scanner(source).scan()
 
@@ -54,5 +67,5 @@ def test_scanner_fails_on_failure_cases(failure_case: WodeTestCase):
         errors[0].error_type == expected_error_types[0]
     ), f"Test case `{test_case_name}` should have returned `{expected_error_types}` error."
 
-    if failure_case.broken:
+    if failure_case[1].broken:
         pytest.xfail()
--- wode/tests/test_3_parser.py
+++ wode/tests/test_3_parser.py
@@ -1,41 +1,55 @@
+from typing import Tuple
+
 import pytest
 
 from wode.ast_to_s_expression import convert_to_s_expression
 from wode.parser import Parser
 from wode.scanner import Scanner
-from wode.tests.conftest import WodeTestCase, test_cases
+from wode.tests.conftest import WodeTestCase, test_cases_flattened
 
-success_cases = [x for x in test_cases if x.expected_ast is not None]
-failure_cases = [x for x in test_cases if x.expected_ast is None]
+success_cases = [
+    (test_case_name, test_case)
+    for test_case_name, test_case in test_cases_flattened
+    if test_case.expected_ast is not None
+]
+failure_cases = [
+    (test_case_name, test_case)
+    for test_case_name, test_case in test_cases_flattened
+    if test_case.expected_ast is None
+]
 
 
 @pytest.mark.parametrize(
-    "success_case", success_cases, ids=[x.name for x in success_cases]
+    "success_case", success_cases, ids=[x[0] for x in success_cases]
 )
-def test_parser(success_case: WodeTestCase):
+def test_parser(success_case: Tuple[str, WodeTestCase]):
+    if success_case[1].broken:
+        pytest.xfail()
+
+    test_case_name = success_case[0]
+    source = success_case[1].source
     # Scan the source
-    tokens, scanner_errors = Scanner(success_case.source).scan()
+    tokens, scanner_errors = Scanner(source).scan()
 
     # If there were any scanner errors, raise them
     if len(scanner_errors) > 0:
-        raise Exception("Scanning errors were found in unit test for parser.")
+        raise Exception(
+            f"Scanning errors were found in unit test `{test_case_name}` for parser."
+        )
 
     # Parse the tokens
-    expressions, parser_errors = Parser(tokens, success_case.source).parse()
+    expressions, parser_errors = Parser(tokens, source).parse()
 
     # If there were any parser errors, raise them
     if len(parser_errors) > 0:
         raise Exception(
             "\n".join(
-                ["Parsing errors found:"]
+                [f"Parsing errors found in test case `{test_case_name}`:"]
                 + [error.get_message() for error in parser_errors]
             )
         )
 
     # Check the parser works as expected
-    expected_ast = success_case.expected_ast
+    expected_ast = success_case[1].expected_ast
     if expected_ast is not None:
         assert [convert_to_s_expression(e) for e in expressions] == expected_ast
-
-    if success_case.broken:
-        pytest.xfail()
