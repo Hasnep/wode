f287d750543cefc167ef31f2f3ffa31f3806ee06
Hannes <h@nnes.dev>
2023-01-06 10:30:40 +0800

    Fix formatting

--- wode/bin/main.ml
+++ wode/bin/main.ml
@@ -1,4 +1,5 @@
 Py.initialize ()
+
 let wode = Py.import "wode"
 let main = Py.Module.get_function wode "main"
-let result  = main ([| (Py.String.of_string ("./example.wode")) |])
+let result = main [| Py.String.of_string "./example.wode" |]
