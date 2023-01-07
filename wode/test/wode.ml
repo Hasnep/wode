b51d6d78bbf122c36f2ce001d206b6931f8f7810
Hannes <h@nnes.dev>
2023-01-07 10:14:20 +0800

    FIXME

--- wode/test/wode.ml
+++ wode/test/wode.ml
@@ -3,7 +3,7 @@ open OUnit2
 let tests =
   "Test suite for wode"
   >::: [
-         ("empty" >:: fun _ -> assert_equal (Wode.sum []) 1);
+         ("empty" >:: fun _ -> assert_equal (Wode.sum []) 0);
          ("singleton" >:: fun _ -> assert_equal (Wode.sum [ 1 ]) 1);
          ("two_elements" >:: fun _ -> assert_equal (Wode.sum [ 1; 2 ]) 3);
        ]
